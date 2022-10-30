a7579620b81a762cbc395353c6ec5b601716ee09
vinay <vinay@LAPTOP-61DQOHIN>
2022-10-30 11:16:18 +0530

    Add project files.

---
+++ dataTypes/IntdataType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypes
+{
+    internal class StringdataType
+    {
+        public static void  MyMethod()
+        {
+            //int x, y, z;
+           // Console.WriteLine("enter x value:");
+            //x = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine("enter y value:");
+           // y = Convert.ToInt32(Console.ReadLine());
+           // z = x + y;
+           // Console.WriteLine(z);
+            //Console.ReadLine();
+            string sname ="durga";
+            Console.WriteLine(sname);
+            //Console.ReadLine();
+
+        }
+        public static void Main()
+        {
+            MyMethod();
+            StringDataType2.MyMethod2();
+           Console.ReadLine(); 
+        }
+
+    }
+}
---
+++ dataTypes/StringDataType2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypes
+{
+    internal class StringDataType2
+    {
+        public static void MyMethod2()
+        {
+            String sname = "Bhanu";
+            Console.WriteLine(sname);
+            Console.ReadLine();
+        }
+        //public static void Main()
+        //{
+        //    MyMethod2();
+        //    Console.ReadLine();
+        //}
+
+    }
+}
